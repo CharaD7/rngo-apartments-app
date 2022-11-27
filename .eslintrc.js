a389ae9a924810f254d3327bfd4db40d30059de6
CharaD7 <jijakahn6@gmail.com>
2022-11-27 21:35:38 +0000

    Added prettier, redefined eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,92 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+    },
+    extends: [
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'plugin:prettier/recommended',
+        // '@react-native-community',
+        'prettier',
+        'prettier/react'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    ignorePatterns: ['!.*', '**/*.js', 'dist', 'node_modules'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 13,
+        project: './tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'react-native', 'jest', 'import'],
+    rules: {
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    ['external', 'builtin'],
+                    'internal',
+                    ['sibling', 'parent'],
+                    'index',
+                ],
+                pathGroups: [
+                    {
+                        pattern: '@(react|react-native)',
+                        group: 'external',
+                        position: 'before',
+                    },
+                    {
+                        pattern: '@src/**',
+                        group: 'internal',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['internal', 'react'],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+        'linebreak-style': 'off',
+        "no-extra-semi": ["error"],
+        "no-use-before-define": "off",
+        // quotes: ['error', 'double'],
+        "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
+        "react-native/no-inline-styles": 2,
+        "react-native/no-color-literals": 2,
+        "semi": [
+            "error",
+            "always",
+            {
+                omitLastInOneLineBlock: false
+            }
+        ],
+        'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+        "@typescript-eslint/no-use-before-define": "error"
+    },
+    "settings": {
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"]
+        },
+        "import/resolver": {
+            typescript: true,
+            node: true
+        },
+        "react": {
+            version: "detect"
+        }
+    }
+};
