c5248ad89ef3041e097dcdda6b5272a89b395ed8
CharaD7 <jijakahn6@gmail.com>
2024-01-18 12:29:33 +0000

    Added map view

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Animated } from 'react-native';
+import { Animated, View } from 'react-native';
 
 import { AnimatedListHeader } from '~components/AnimatedHeader';
 import { Card } from '~components/Card';
 import { Screen } from '~components/Screen';
 import { HEADERHEIGHT, LISTMARGIN } from '~constants/ScreenConstants';
+
+import MapView from 'react-native-maps';
 
 const SearchScreen = () => {
   const properties = [
@@ -66,31 +68,43 @@ const SearchScreen = () => {
   ];
 
   const [scrollAnimation] = useState(new Animated.Value(0));
+  const [mapShown, setMapShown] = useState<boolean>(false);
+
   return (
     <Screen>
-      <AnimatedListHeader scrollAnimation={scrollAnimation} />
-      <Animated.FlatList
-        bounces={false}
-        contentContainerStyle={{ paddingTop: HEADERHEIGHT - 20 }}
-        data={properties}
-        keyExtractor={(item) => item.id.toString()}
-        onScroll={Animated.event(
-          [
-            {
-              nativeEvent: {
-                contentOffset: {
-                  y: scrollAnimation,
+      <AnimatedListHeader
+        scrollAnimation={scrollAnimation}
+        setMapShown={setMapShown}
+        mapShown={mapShown}
+      />
+      {mapShown ? (
+        <View style={{ flex: 1, overflow: 'hidden' }}>
+          <MapView style={{ height: '100%', width: '100%' }} />
+        </View>
+      ) : (
+        <Animated.FlatList
+          bounces={false}
+          contentContainerStyle={{ paddingTop: HEADERHEIGHT - 20 }}
+          data={properties}
+          keyExtractor={(item) => item.id.toString()}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    y: scrollAnimation,
+                  },
                 },
               },
-            },
-          ],
-          { useNativeDriver: true },
-        )}
-        renderItem={({ item }) => <Card style={{ marginVertical: 5 }} property={item} />}
-        scrollEventThrottle={16}
-        showsHorizontalScrollIndicator={false}
-        style={{ marginHorizontal: LISTMARGIN }}
-      />
+            ],
+            { useNativeDriver: true },
+          )}
+          renderItem={({ item }) => <Card style={{ marginVertical: 5 }} property={item} />}
+          scrollEventThrottle={16}
+          showsHorizontalScrollIndicator={false}
+          style={{ marginHorizontal: LISTMARGIN }}
+        />
+      )}
     </Screen>
   );
 };
