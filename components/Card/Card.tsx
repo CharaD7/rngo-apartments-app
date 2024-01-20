68e19cce6713f58644100482f878e225120d226c
CharaD7 <jijakahn6@gmail.com>
2024-01-20 16:04:54 +0000

    Restyle card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { View, ViewStyle } from 'react-native';
+import { StyleSheet, View, ViewStyle } from 'react-native';
 
 import { CardMeta } from '~components/CardInfo';
 import { Carousel } from '~components/Carousel';
+import { LISTMARGIN } from '~constants/ScreenConstants';
 import { Property } from '~types/property';
 
 const Card = ({ property, style }: { property: Property; style?: ViewStyle }) => {
   return (
-    <View style={style}>
+    <View style={[styles.container, style]}>
       <Carousel images={property.images} />
       <CardMeta property={property} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: LISTMARGIN,
+    borderRadius: 5,
+    backgroundColor: 'white',
+  },
+});
 
 export default Card;
