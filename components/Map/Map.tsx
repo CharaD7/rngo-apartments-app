43620081ce420fe777dc142749e3112f2dea7000
CharaD7 <jijakahn6@gmail.com>
2024-01-20 12:30:56 +0000

    Add card

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import { Platform, StyleSheet, View } from 'react-native';
 
 import { theme } from 'theme';
+import { Card } from '~components/Card';
 import { Property } from '~types/property';
 
 import { useNavigation } from '@react-navigation/native';
@@ -44,11 +45,18 @@ const Map = ({ properties }: { properties: Property[] }) => {
           />
         ))}
       </MapView>
+
+      {activeIndex > -1 && <Card property={properties[activeIndex]} style={styles.card} />}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  card: {
+    bottom: 10,
+    height: 360,
+    position: 'absolute',
+  },
   container: {
     flex: 1,
     overflow: 'hidden',
